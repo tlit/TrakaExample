552fbd19700cf243040573020d5a627d89815a4f
Tom Littlewood <tom.littlewood@gmail.com>
2020-08-30 13:27:53 +0100

    Initial commit

---
+++ UserOperations.feature
@@ -0,0 +1,50 @@
+﻿Feature: UserOperations
+	In order to administer Users of the system
+	I wish to perform operations on the User List
+
+@smoketest
+Scenario Outline: Return pages of the User List
+	When I request page <pageNumber> of the User List
+	Then the response code is 200
+	And the response page is <pageNumber>
+	And the response includes <perPage> items per page
+	And the response indicates <totalItems> items in total
+	And the response indicates <totalPages> pages in total
+	And the response data includes <itemCount> Users
+Examples: 
+| pageNumber | perPage | totalItems | totalPages | itemCount |
+| 1          | 6       | 12         | 2          | 6         |
+| 2          | 6       | 12         | 2          | 6         |
+| 200        | 6       | 12         | 2          | 0         |
+
+@smoketest
+Scenario Outline: Return a single User
+	When I request details for User ID <UserId>
+	Then the response code is 200
+	And the response contains a single User
+	And the User's first name is <firstName>
+	And the User's last name is <lastName>
+	And the User's email address is <email>
+	And the User's avatar is a valid URI
+Examples:
+| UserId | firstName | lastName | email                  |
+| 1      | George    | Bluth    | george.bluth@reqres.in |
+| 4      | Eve       | Holt     | eve.holt@reqres.in     |
+| 9      | Tobias    | Funke    | tobias.funke@reqres.in |
+
+@smoketest
+Scenario: Attempt to return an invalid User
+	When I request details for User ID 23
+	Then the response code is 404
+	
+@smoketest
+Scenario Outline: Add a User
+	When I add the User '<firstName> <lastName>'
+	Then the response code is 201
+	And the User's first name is <firstName>
+	And the User's last name is <lastName>
+Examples:
+| UserId	| firstName | lastName |
+| 55		| Leann     | Hope     |
+| 28		| Anita     | Lemon    |
+| 12		| Katja     | Fish     |
---
+++ UserOperations_steps.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+using System.Net;
+using System.Collections.Generic;
+
+namespace ApiTestAutomation_01
+{
+    public class Context
+    {
+        public HttpResponseMessage response;
+        public User user;
+        public UserListResponse userListResponse;
+        public UserQueryResponse userQueryResponse;
+    }
+
+    public class User
+    {
+        public int id;
+        public string email;
+        public string first_name;
+        public string last_name;
+        public string avatar;
+    }
+
+    public class UserListResponse
+    {
+        public int page;
+        public int per_page;
+        public int total;
+        public int total_pages;
+        public User[] data;
+    }
+
+    public class UserQueryResponse
+    {
+        public User data;
+    }
+
+    [Binding]
+    public class UserOperations_steps
+    {
+        static HttpClient client = new HttpClient();
+        private readonly Uri baseUri = new Uri("https://reqres.in/api/");
+        private Context context;
+        
+        public UserOperations_steps(Context context)
+        {
+            this.context = context;
+        }
+
+
+
+        [When(@"I request page (.*) of the User List")]
+        public async Task WhenIRequestPageOfTheUserList(int pageNumber)
+        {
+            string path = $"users?page={pageNumber.ToString()}";
+            string fullUri = $"{this.baseUri}{path}";
+            context.response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, fullUri));
+            try
+            {
+                context.userListResponse = await context.response.Content.ReadAsAsync<UserListResponse>();
+            }
+            finally
+            {
+            }
+        }
+
+        [When(@"I request details for User ID (.*)")]
+        public async Task WhenIRequestDetailsForUserID(int userId)
+        {
+            string path = $"users/{userId}";
+            string fullUri = $"{this.baseUri}{path}";
+            context.response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, fullUri));
+            try
+            {
+                context.userQueryResponse = await context.response.Content.ReadAsAsync<UserQueryResponse>();
+                context.user = context.userQueryResponse.data;
+            }
+            finally
+            {
+            }
+        }
+
+        [When(@"I add the User '(.*) (.*)'")]
+        public async Task WhenIAddTheUser(string firstName, string lastName)
+        {
+            string path = $"users";
+            string fullUri = $"{this.baseUri}{path}";
+            HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Post, fullUri);
+            msg.Content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("first_name", firstName),
+                new KeyValuePair<string, string>("last_name", lastName),
+            });
+            context.response = await client.SendAsync(msg);
+            try
+            {
+                context.user = await context.response.Content.ReadAsAsync<User>();
+            }
+            finally
+            {
+            }
+        }
+
+
+
+        [Then(@"the response code is (.*)")]
+        public void ThenTheResponseCodeIs(int responseCodeExpected)
+        {
+            HttpStatusCode statusCode = HttpStatusCode.OK;
+            switch (responseCodeExpected)
+            {
+                case 200:
+                    statusCode = HttpStatusCode.OK;
+                    break;
+                case 201:
+                    statusCode = HttpStatusCode.Created;
+                    break;
+                case 400:
+                    statusCode = HttpStatusCode.BadRequest;
+                    break;
+                case 404:
+                    statusCode = HttpStatusCode.NotFound;
+                    break;
+                case 500:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    break;
+            }
+            context.response.StatusCode.Should().Be(statusCode);
+        }
+
+        [Then(@"the response page is (.*)")]
+        public void ThenTheResponsePageIs(int pageNumberExpected)
+        {
+            context.userListResponse.page.Should().Be(pageNumberExpected);
+        }
+
+        [Then(@"the response includes (.*) items per page")]
+        public void ThenTheResponseIncludesItemsPerPage(int perPageExpected)
+        {
+            context.userListResponse.per_page.Should().Be(perPageExpected);
+        }
+
+        [Then(@"the response indicates (.*) items in total")]
+        public void ThenTheResponseIndicatesItemsInTotal(int totalExpected)
+        {
+            context.userListResponse.total.Should().Be(totalExpected);
+        }
+
+        [Then(@"the response indicates (.*) pages in total")]
+        public void ThenTheResponseIndicatesPagesInTotal(int totalPagesExpected)
+        {
+            context.userListResponse.total_pages.Should().Be(totalPagesExpected);
+        }
+
+        [Then(@"the response data includes (.*) users")]
+        public void ThenTheResponseDataIncludesUsers(int userCountExpected)
+        {
+            context.userListResponse.data.Length.Should().Be(userCountExpected);
+        }
+
+        [Then(@"the response contains a single user")]
+        public void ThenTheResponseContainsASingleUser()
+        {
+            context.userQueryResponse.data.Should().BeOfType<User>();
+        }
+
+        [Then(@"the User's first name is (.*)")]
+        public void ThenTheUserSFirstNameIs(string firstNameExpected)
+        {
+            context.user.first_name.Should().Be(firstNameExpected);
+        }
+
+        [Then(@"the User's last name is (.*)")]
+        public void ThenTheUserSLastNameIs(string lastNameExpected)
+        {
+            context.user.last_name.Should().Be(lastNameExpected);
+        }
+
+        [Then(@"the User's email address is (.*)")]
+        public void ThenTheUserSEmailAddressIs(string emailExpected)
+        {
+            context.user.email.Should().Be(emailExpected);
+        }
+
+        [Then(@"the User's avatar is a valid URI")]
+        public void ThenTheUserSAvatarIsAValidURI()
+        {
+            Uri avatarUri = new Uri(context.user.avatar);
+            avatarUri.Should().BeOfType<Uri>();
+        }
+    }
+}
